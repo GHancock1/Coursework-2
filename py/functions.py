5b16aa781f12b94966f8df97a4680467e9261a94
GHancock1 <gahancock29@gmail.com>
2025-12-09 17:49:55 +0000

    9/12 end of day

--- py/functions.py
+++ py/functions.py
@@ -1,1 +1,8 @@
+import pandas as pd
 
+# Function to fetch dataset from github and remove empty columns (such as the errors of the stellar age)
+def getData():
+    df = pd.read_csv("https://raw.githubusercontent.com/GHancock1/Coursework-2/main/KESR.csv", sep=",", header=0,)
+    df.dropna(axis = 1, inplace=True, how = "all")
+    df.drop(list(df.filter(regex = 'err')), axis = 1, inplace = True)
+    return df
